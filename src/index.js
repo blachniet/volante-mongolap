227ad289fe16ad9e281ba75bc5e65681427bb5df
Mike Smiley <msmiley@alumni.rice.edu>
2023-09-13 14:12:50 -0500

    fix regex call

--- src/index.js
+++ src/index.js
@@ -343,7 +343,7 @@ module.exports = {
         match[this.timestampField] = { $exists: true, $type: 'date' };
       }
       if (regex) {
-        match[this.field] = { $regex: this.regex, $options: 'i' };
+        match[this.field] = { $regex: regex, $options: 'i' };
       }
       let pipeline = [
         { $match: match },
